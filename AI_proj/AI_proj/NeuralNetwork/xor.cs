35333aadd9a2c330c275c4525e70f5468f490341
admal <adam.malewski@wp.pl>
2016-05-15 19:30:25 +0300

    number comparing neural network + tests

--- AI_proj/AI_proj/NeuralNetwork/xor.cs
+++ AI_proj/AI_proj/NeuralNetwork/xor.cs
@@ -17,7 +17,10 @@ namespace AI_proj.NeuralNetwork
         public static NeuralNet get_xor_net()
         {
            
-            NeuralNet net = new NeuralNet(3, 2, 3, 1); // layers, input, hidden, output
+            //NeuralNet net = new NeuralNet(3, 2, 3, 1); // layers, input, hidden, output
+            NeuralNet net = new NeuralNet(NetworkType.LAYER,new List<uint>() {2,10,1});
+         //   NeuralNet n = new NeuralNet();
+            
             //Activation function layers
             /*net.SetActivationFunctionLayer(ActivationFunction.SIGMOID_SYMMETRIC, 2);
             net.SetActivationFunctionLayer(ActivationFunction.SIGMOID_SYMMETRIC, (int)net.LayerCount-1);*/
@@ -27,40 +30,91 @@ namespace AI_proj.NeuralNetwork
 
             create_training_file();
 
-            TrainingData data = new TrainingData("C:/Users/Jedrek/Documents/Studies/AI/AI_proj/AI_proj/AI_proj/NeuralNetwork/xor.data");
+            TrainingData data = new TrainingData(@"C:\Users\Adam\Documents\AIProj\xor.data");
 
-            net.TrainOnData(data, 500000, 1000, (float)0.001);
+            net.TrainOnData(data, 500000, 1000, 0.0f);
             
             //net.Test(new double[] { 0, 0 }, new double[] { 0 });
             //net.Test(new double[] { 0, 1 }, new double[] { 1 });
             //net.Test(new double[] { 1, 0 }, new double[] { 1 });
             //net.Test(new double[] { 1, 1 }, new double[] { 0 });
 
-            net.Save("C:/Users/Jedrek/Documents/Studies/AI/AI_proj/AI_proj/AI_proj/NeuralNetwork/xor_learned.net");
+            net.Save(@"C:\Users\Adam\Documents\AIProj\xor_learned.net");
 
             return net;
         }
 
+        //private static void create_training_file()
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    sb.AppendLine("8 2 1");
+
+        //    sb.AppendLine("0 1");
+        //    sb.AppendLine("1");
+        //    sb.AppendLine("0 0");
+        //    sb.AppendLine("0");
+        //    sb.AppendLine("0 -1");
+        //    sb.AppendLine("0");
+
+        //    sb.AppendLine("1 1");
+        //    sb.AppendLine("1");
+        //    sb.AppendLine("1 -1");
+        //    sb.AppendLine("1");
+        //    sb.AppendLine("1 0");
+        //    sb.AppendLine("1");
+
+        //    sb.AppendLine("-1 -1");
+        //    sb.AppendLine("-1");
+        //    sb.AppendLine("-1 0");
+        //    sb.AppendLine("0");
+        //    //sb.AppendLine("-1 1");
+        //    //sb.AppendLine("1");
+        //    File.WriteAllText(@"C:\Users\Adam\Documents\AIProj\xor.data", sb.ToString());
+        //}
         private static void create_training_file()
         {
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine("4 2 1");
-            sb.AppendLine("0 0");
-            sb.AppendLine("1");
-            sb.AppendLine("0 1");
-            sb.AppendLine("0");
-            sb.AppendLine("1 0");
-            sb.AppendLine("0");
-            sb.AppendLine("1 1");
-            sb.AppendLine("0");
-            File.WriteAllText("C:/Users/Jedrek/Documents/Studies/AI/AI_proj/AI_proj/AI_proj/NeuralNetwork/xor.data", sb.ToString());
-        }
+            sb.AppendLine("10 2 1");
 
+            sb.AppendLine("0 1");
+            sb.AppendLine("-1");
+            sb.AppendLine("0 0");
+            sb.AppendLine("0");
+            sb.AppendLine("0 -1");
+            sb.AppendLine("1");
+
+            sb.AppendLine("5 1");
+            sb.AppendLine("1");
+            sb.AppendLine("100 20");
+            sb.AppendLine("1");
+            sb.AppendLine("39 -21");
+            sb.AppendLine("1");
+
+            sb.AppendLine("-23 -21");
+            sb.AppendLine("-1");
+            sb.AppendLine("-10 21");
+            sb.AppendLine("-1");
+
+            sb.AppendLine("-10 -12");
+            sb.AppendLine("1");
+            sb.AppendLine("-20 30");
+            sb.AppendLine("-1");
+            //sb.AppendLine("-1 1");
+            //sb.AppendLine("1");
+            File.WriteAllText(@"C:\Users\Adam\Documents\AIProj\xor.data", sb.ToString());
+        }
         public static void Test(NeuralNet net, double a, double b)
         {
+            int res = a > b ? 1 : -1;
+            if (Math.Abs(a - b) < Double.Epsilon)
+            {
+                res = 0;
+            }
             double r = net.Run(new double[] { a, b })[0];
-            r = Math.Round(r, 1);
-            Debug.WriteLine("{0} xor {1} = {2}", a, b, r);
+            r = Math.Round(r, 8);
+            //Console.WriteLine("{0} xor {1} = {2}", a, b, r);
+
+            Console.WriteLine(a +" > "+b+"; "+r + "  ;" + (Math.Abs(res - r) < 0.1));
         }
     }
 }
---
+++ AI_proj/NetworkTests/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using AI_proj.NeuralNetwork;
+using FANNCSharp.Double;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetworkTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("XOR");
+            //NeuralNet xornet = XOR.get_xor_net();
+            //XOR.Test(xornet, -1, -1);
+            //XOR.Test(xornet, -1, 1);
+            //XOR.Test(xornet, -1, 0);
+            //Console.WriteLine();
+            //XOR.Test(xornet, 1, -1);
+            //XOR.Test(xornet, 1, 1);
+            //XOR.Test(xornet, 1, 0);
+            //Console.WriteLine();
+            //XOR.Test(xornet, 0, -1);
+            //XOR.Test(xornet, 0, 1);
+            //XOR.Test(xornet, 0, 0);
+
+            NeuralNet xornet = XOR.get_xor_net();
+            XOR.Test(xornet, -1, -1);
+            XOR.Test(xornet, -1, 1);
+            XOR.Test(xornet, -1, 0);
+            Console.WriteLine();
+            XOR.Test(xornet, 1, -1);
+            XOR.Test(xornet, 1, 1);
+            XOR.Test(xornet, 1, 0);
+            Console.WriteLine();
+            XOR.Test(xornet, 0, -1);
+            XOR.Test(xornet, 0, 1);
+            XOR.Test(xornet, 0, 0);
+
+            Console.WriteLine();
+            XOR.Test(xornet, 100, 2);
+            XOR.Test(xornet, -12, 3);
+            XOR.Test(xornet, 22, 1010101);
+
+            Console.WriteLine();
+            XOR.Test(xornet, 12.5, 12);
+            XOR.Test(xornet, 10, -10);
+            XOR.Test(xornet, -19.5, -19);
+            XOR.Test(xornet, 1234.0001, 1234);
+            XOR.Test(xornet, 5, 5+2*Double.Epsilon);
+
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                double x = rand.NextDouble(), y = rand.NextDouble();
+                int xOfsset = rand.Next(-1000, 1000), yOffset = rand.Next(-1000, 1000);
+                XOR.Test(xornet, x + xOfsset, y + yOffset);
+            }
+        }
+    }
+}
