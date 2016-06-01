348ba38c5cb9b4c28372d4292940111680a7577e
Jan Baraniewski <janekbaraniewski@gmail.com>
2016-06-01 14:18:45 +0200

    UploadImage model, no uploading yet

--- AI_proj/AI_proj/Controllers/NeuralClassifierController.cs
+++ AI_proj/AI_proj/Controllers/NeuralClassifierController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AI_proj.Models;
 
 namespace AI_proj.Controllers
 {
@@ -13,5 +17,46 @@ namespace AI_proj.Controllers
         {
             return View();
         }
+        // POST: NeuralClassifier
+        [HttpPost]
+        public ActionResult Index(UploadImageModel model)
+        {
+                Bitmap original = null;
+                var name = "newimagefile";
+                var errorField = string.Empty;
+
+                errorField = "File";
+                name = Path.GetFileNameWithoutExtension(model.File.FileName);
+                original = Bitmap.FromStream(model.File.InputStream) as Bitmap;
+
+
+                if (original != null)
+                {
+                    var fn = Server.MapPath("~/Content/img/" + name + ".png");
+                    var img = CreateImage(original, model.X, model.Y, model.Width, model.Height);
+                    img.Save(fn, System.Drawing.Imaging.ImageFormat.Png);
+                    return RedirectToAction("Index");
+                }
+                else
+                    ModelState.AddModelError(errorField, "Your upload did not seem valid. Please try again using only correct images!");
+        
+            return View(model);
+        }
+
+        Bitmap CreateImage(Bitmap original, int x, int y, int width, int height)
+        {
+            var img = new Bitmap(width, height);
+
+            using (var g = Graphics.FromImage(img))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(original, new Rectangle(0, 0, width, height), x, y, width, height, GraphicsUnit.Pixel);
+            }
+
+            return img;
+        }
+
+
     }
 }
---
+++ AI_proj/AI_proj/Models/NeuralClassifierModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace AI_proj.Models
+{
+
+    public class UploadImageModel
+    {
+        [Display(Name = "Local file")]
+        public HttpPostedFileBase File { get; set; }
+
+        public bool IsFile { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int X { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Y { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Width { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Height { get; set; }
+
+    }
+}
