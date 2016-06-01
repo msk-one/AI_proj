ab1f29284788118523f766ca51c974f2144b22de
Jan Baraniewski <janekbaraniewski@gmail.com>
2016-06-01 13:28:21 +0200

    Image upload preview

---
+++ AI_proj/AI_proj/Controllers/NeuralClassifierController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AI_proj.Controllers
+{
+    public class NeuralClassifierController : Controller
+    {
+        // GET: NeuralClassifier
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
