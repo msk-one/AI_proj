434582bf0ad0e06bf3d39eb8412c9d179ddf42c7
Msk <msk.sk.one@gmail.com>
2016-06-08 16:12:39 +0200

    Canvas instead of file upload. First version.

---
+++ AI_proj/AI_proj/Content/canvasHandler.js
@@ -0,0 +1,72 @@
+﻿var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+var canvasDiv = document.getElementById('canvasDiv');
+canvas = document.createElement('canvas');
+canvas.setAttribute('width', 500);
+canvas.setAttribute('height', 500);
+canvas.setAttribute('id', 'canvas');
+canvasDiv.appendChild(canvas);
+if (typeof G_vmlCanvasManager != 'undefined') {
+    canvas = G_vmlCanvasManager.initElement(canvas);
+}
+context = canvas.getContext("2d");
+
+function clearCanvas() {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    clickX = new Array();
+    clickY = new Array();
+    clickDrag = new Array();
+}
+
+function addClick(x, y, dragging) {
+    clickX.push(x-75);
+    clickY.push(y-85);
+    clickDrag.push(dragging);
+}
+
+function redraw() {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); 
+
+    context.strokeStyle = "#000000";
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+
+    for (var i = 0; i < clickX.length; i++) {
+        context.beginPath();
+        if (clickDrag[i] && i) {
+            context.moveTo(clickX[i - 1], clickY[i - 1]);
+        } else {
+            context.moveTo(clickX[i] - 1, clickY[i]);
+        }
+        context.lineTo(clickX[i], clickY[i]);
+        context.closePath();
+        context.stroke();
+    }
+}
+
+$('#canvas').mousedown(function (e) {
+    var mouseX = e.pageX - this.offsetLeft;
+    var mouseY = e.pageY - this.offsetTop;
+		
+    paint = true;
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+    redraw();
+});
+
+$('#canvas').mousemove(function (e) {
+    if (paint) {
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+        redraw();
+    }
+});
+
+$('#canvas').mouseup(function (e) {
+    paint = false;
+});
+
+$('#canvas').mouseleave(function (e) {
+    paint = false;
+});
